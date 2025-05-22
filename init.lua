171edaa1b6d9b6779cd3a722337d982ec29177b4
Steve Smaldone <smaldone@gmail.com>
2025-05-22 08:32:52 -0400

    Added Flog plug for git graph view

--- init.lua
+++ init.lua
@@ -1045,6 +1045,15 @@ require('lazy').setup({
     'github/copilot.vim',
   },
 
+  {
+    'rbong/vim-flog',
+    lazy = true,
+    cmd = { 'Flog', 'Flogsplit', 'Floggit' },
+    dependencies = {
+      'tpope/vim-fugitive',
+    },
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
